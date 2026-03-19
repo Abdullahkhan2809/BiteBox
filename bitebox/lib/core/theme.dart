414671797f50fc1587bfd0b38d050a54d9ece225
Abdullah Khan <abdullahadil108@gmail.com>
2026-03-20 04:33:01 +0500

    commit

---
+++ bitebox/lib/core/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: Color(0xFFFF6B35),
+    fontFamily: 'Poppins',
+    // button styles, input decorations, etc.
+  );
+}
